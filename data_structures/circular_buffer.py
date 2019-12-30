52619680f908659e41bb9fd2cb6f03766c9fbdee
Roland Gavrilescu <gavrilescu.roland@gmail.com>
2019-12-30 14:52:12 +0200

    stack and buffer

---
+++ data_structures/circular_buffer.py
@@ -0,0 +1,52 @@
+"""
+    Implementation of a circular buffer of fixed storage size.
+
+"""
+class CircularBuffer:
+    
+    def __init__(self, size):
+        self.buffer = [None]*size
+        self.low = 0    # where to remove from
+        self.high = 0   # where to add to
+        self.size = size
+        self.count = 0
+
+    def is_empty(self):
+        return self.count == 0
+
+    def is_full(self):
+        return self.count == self.size
+        
+    def __len__(self):
+        return self.count
+        
+    def add(self, value):
+        if self.is_full():
+            self.low = (self.low + 1) % self.size
+        else:
+            self.count += 1
+        self.buffer[self.high] = value
+        self.high = (self.high + 1) % self.size
+    
+    def remove(self):
+        if self.count == 0:
+          raise Exception ("Circular Buffer is empty")
+        value = self.buffer[self.low]
+        self.low = (self.low + 1) % self.size
+        self.count -= 1
+        return value
+    
+    def __iter__(self):
+        idx = self.low
+        num = self.count
+        while num > 0:
+            yield self.buffer[idx]
+            idx = (idx + 1) % self.size
+            num -= 1
+
+    def __repr__(self):
+        if self.is_empty():
+          return 'buffer:[]'
+        return 'buffer:[' + ','.join(map(str,self)) + ']'
+
+
---
+++ data_structures/stack.py
@@ -0,0 +1,23 @@
+"""
+    Implementation of a stack.
+    
+"""
+class Stack:
+  
+  def __init__(self):
+    self.stack = []
+
+  def is_empty(self):
+    return len(self.stack) == 0
+
+  def push(self, x):
+    self.stack.append(x)
+
+  def pop(self):
+    if self.is_empty(): 
+      raise Exception('Stack is empty.')
+    return self.stack.pop()
+
+  def __repr__(self):
+    return 'stack:' + str(self.stack) 
+
